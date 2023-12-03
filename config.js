a6f424f5f44aebea65ebd80db642e734a22b563f
Kisibo <149688266+Kisibo@users.noreply.github.com>
2023-12-03 13:48:08 +0300

    Update config.js

--- config.js
+++ config.js
@@ -5,22 +5,16 @@ import fs from 'fs'
 import fetch from 'node-fetch'
 import axios from 'axios'
 
-
-global.owner = [
-  ['254716838981', 'phillo⚡️²⁰²³', true],
-  ['12104605375 ', '~ 𝐈𝐭𝐬_𝐒𝐢𝐫𝐦™𝕏', true], 
-  [''] 
-] //Number of owners
-
-global.mods = ['0716838981','0716838989'] 
-global.prems = ['0716838981', '0716838981', '0716838981']
-global.keysZens = ['c2459db922', '37CC845916', '6fb0eff124']
-global.keysxxx = keysZens[Math.floor(keysZens.length * Math.random())]
-global.keysxteammm = ['29d4b59a4aa687ca', '5LTV57azwaid7dXfz5fzJu', 'cb15ed422c71a2fb', '5bd33b276d41d6b4', 'HIRO', 'kurrxd09', 'ebb6251cc00f9c63']
-global.keysxteam = keysxteammm[Math.floor(keysxteammm.length * Math.random())]
-global.keysneoxrrr = ['5VC9rvNx', 'cfALv5']
-global.keysneoxr = keysneoxrrr[Math.floor(keysneoxrrr.length * Math.random())]
-global.lolkeysapi = ['BrunoSobrino']
+  [''] const fs = require('fs');
+const { Sequelize } = require('sequelize');
+const isVPS = !(__dirname.startsWith("/rgnk") || __dirname.startsWith("/skl"));
+const isHeroku = __dirname.startsWith("/skl");
+const isKoyeb = __dirname.startsWith("/rgnk");
+const isRailway = __dirname.startsWith("/railway");
+if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env' });
+function convertToBool(text, fault = 'true',fault2='on') {
+    return ((text === fault) || (text === fault2));
+}
 const settingsMenu = [
     {title: "PM antispam block", env_var: "PM_ANTISPAM"},
     {title: "Auto read all messages", env_var: "READ_MESSAGES"},
@@ -32,68 +26,64 @@ const settingsMenu = [
     {title: "Always online", env_var: "ALWAYS_ONLINE"},
     {title: "PM Auto blocker", env_var: "PMB_VAR"},
     {title: "Disable bot in PM", env_var: "DIS_PM"}
-global.APIs = { // API Prefix
-  // name: 'https://website'
-  xteam: 'https://api.xteam.xyz', 
-  dzx: 'https://api.dhamzxploit.my.id',
-  lol: 'https://api.lolhuman.xyz',
-  violetics: 'https://violetics.pw',
-  neoxr: 'https://api.neoxr.my.id',
-  zenzapis: 'https://zenzapis.xyz',
-  akuari: 'https://api.akuari.my.id',
-  akuari2: 'https://apimu.my.id',
-  nrtm: 'https://fg-nrtm.ddns.net',
-  bg: 'http://bochil.ddns.net',
-  fgmods: 'https://api-fgmods.ddns.net'
-}
-global.APIKeys = { // APIKey Here
-  // 'https://website': 'apikey'
-  'https://api.xteam.xyz': 'd90a9e986e18778b',
-  'https://api.lolhuman.xyz': '85faf717d0545d14074659ad',
-  'https://api.neoxr.my.id': `${keysneoxr}`,	
-  'https://violetics.pw': 'beta',
-  'https://zenzapis.xyz': `${keysxxx}`, 
-  'https://api-fgmods.ddns.net': 'fg-dylux'
-}
-STICKER_DATA: process.env.STICKER_DATA || "Phillo⚡️",
-global.botname = '💝 Phillo⚡️bot 💝'
-global.premium = 'true'
-global.packname = '💝 Phillo⚡️💝 Created By 💝 Phillo²⁰²³ 💝' 
-global.author = '💝 Phillo²⁰²³ 💝' 
-global.fgsc = 'https://github.com/dinuwah/QUEEN-HENTAI' 
-global.fglog = 'https://i.ibb.co/T1DqdYz/thumbnail.jpg' 
-BOT_INFO: process.env.BOT_INFO || 254716838981
+global.owner = [
+  ['254716838981', 'Phillo⚡️', true],
 
-global.wait = '*💝 _Waiting  Uploading by 💝 Phillo⚡️ 💝..._*'
-global.rwait = '⏳️'
-global.dmoji = '😁'
-global.done = '👍'
-global.error = '👎' 
-global.xmoji = '🫶' 
-
-global.multiplier = 69 
-global.maxwarn = '3' // máxima advertencias
-
-let file = fileURLToPath(import.meta.url)
-watchFile(file, () => {
-  unwatchFile(file)
-  console.log(chalk.redBright("Update 'config.js'"))
-  import(`${file}?update=${Date.now()}`)
-}
-
-// Sticker WM
-global.botname = 'The phillo-bot'
-global.premium = 'true'
-global.packname = 'phillo ┃ᴮᴼᵀ' 
-global.author = '@philokisibo254@gmail.com 
-global.multiplier = 69 
-global.maxwarn = '3' // máxima advertencias
-
-let file = fileURLToPath(import.meta.url)
-watchFile(file, () => {
-  unwatchFile(file)
-  console.log(chalk.redBright("Update 'config.js'"))
-  import(`${file}?update=${Date.now()}`)
-})
-
-${format(e)}
+DATABASE_URL = process.env.DATABASE_URL === undefined ? './bot.db' : process.env.DATABASE_URL;
+DEBUG = process.env.DEBUG === undefined ? false : convertToBool(process.env.DEBUG);
+if (!(process.env.SESSION || process.env.SESSION_ID)) throw new Error("No session found, add session before starting bot")
+module.exports = {
+    VERSION: 'v4.0.0',
+    ALIVE: process.env.ALIVE || "https://i.imgur.com/KCnoMM2.jpg Hey {sender}, I'm alive \n Uptime: {uptime}",
+    BLOCK_CHAT: process.env.BLOCK_CHAT || '',
+    PM_ANTISPAM: convertToBool(process.env.PM_ANTISPAM) || '',
+    ALWAYS_ONLINE: convertToBool(process.env.ALWAYS_ONLINE) || false,
+    MANGLISH_CHATBOT: convertToBool(process.env.MANGLISH_CHATBOT) || false,
+    ADMIN_ACCESS: convertToBool(process.env.ADMIN_ACCESS) || false,
+    PLATFORM:isHeroku?"Heroku":isRailway?"Railway":isKoyeb?"Koyeb":"Other server",isHeroku,isKoyeb,isVPS,isRailway,
+    AUTOMUTE_MSG: process.env.AUTOMUTE_MSG || '_Group automuted!_\n_(edit AUTOMUTE_MSG)_',
+    ANTIWORD_WARN: process.env.ANTIWORD_WARN || '',
+    ANTI_SPAM: process.env.ANTI_SPAM || '919074309534-1632403322@g.us',
+    MULTI_HANDLERS: convertToBool(process.env.MULTI_HANDLERS) || false,
+    NOLOG: process.env.NOLOG || false,
+    DISABLED_COMMANDS: (process.env.DISABLED_COMMANDS ? process.env.DISABLED_COMMANDS.split(",") : undefined) || [],
+    ANTI_BOT: process.env.ANTI_BOT || '',
+    ANTISPAM_COUNT: process.env.ANTISPAM_COUNT || '6/10', // msgs/sec
+    AUTOUNMUTE_MSG: process.env.AUTOUNMUTE_MSG || '_Group auto unmuted!_\n_(edit AUTOUNMUTE_MSG)_',
+    AUTO_READ_STATUS: convertToBool(process.env.AUTO_READ_STATUS) || false,
+    READ_MESSAGES: convertToBool(process.env.READ_MESSAGES) || false,
+    PMB_VAR: convertToBool(process.env.PMB_VAR) || false,
+    DIS_PM: convertToBool(process.env.DIS_PM) || false,
+    REJECT_CALLS: convertToBool(process.env.REJECT_CALLS) || false,
+    PMB: process.env.PMB || '_Personal messages not allowed, BLOCKED!_',
+    READ_COMMAND: convertToBool(process.env.READ_COMMAND) || true,
+    SESSION: (process.env.SESSION || process.env.SESSION_ID || '').trim() || '',
+    IMGBB_KEY: ["76a050f031972d9f27e329d767dd988f", "deb80cd12ababea1c9b9a8ad6ce3fab2", "78c84c62b32a88e86daf87dd509a657a"],
+    RG: process.env.RG || '919074309534-1632403322@g.us,120363116963909366@g.us',
+    BOT_INFO: process.env.BOT_INFO || 'Raganork;Skl11;0;https://i.imgur.com/P7ziVhr.jpeg;https://chat.whatsapp.com/Dt3C4wrQmt0GG6io1IBIHb',
+    RBG_KEY: process.env.RBG_KEY || '',
+    ALLOWED: process.env.ALLOWED || '91,94,2',
+    NOT_ALLOWED: process.env.ALLOWED || '91,94,212',
+    CHATBOT: process.env.CHATBOT || 'off',
+    HANDLERS: process.env.HANDLERS || '.,',
+    STICKER_DATA: process.env.STICKER_DATA || "Raganork",
+    BOT_NAME: process.env.BOT_NAME || 'Raganork',
+    AUDIO_DATA: process.env.AUDIO_DATA === undefined || process.env.AUDIO_DATA === "private" ? 'êª¶Í¢Ù­ð‘ºðœ£ð‘¼ð‘¹ð›¢ð‘½ð‘²ð‘³Â¹Â¹ê«‚;Raganork MD bot;https://i.imgur.com/P7ziVhr.jpeg' : process.env.AUDIO_DATA,
+    TAKE_KEY: process.env.TAKE_KEY || '',
+    MODE: process.env.MODE || 'private',
+    WARN: process.env.WARN || '4',
+    ANTILINK_WARN: process.env.ANTILINK_WARN || '',
+    HEROKU: {
+        HEROKU: process.env.HEROKU === undefined ? false : convertToBool(process.env.HEROKU),
+        API_KEY: process.env.HEROKU_API_KEY || '',
+        APP_NAME: process.env.HEROKU_APP_NAME || ''
+    },
+    DATABASE_URL: DATABASE_URL,
+    DATABASE: DATABASE_URL === './bot.db' ? new Sequelize({ dialect: "sqlite", storage: DATABASE_URL, logging: DEBUG }) : new Sequelize(DATABASE_URL, { dialectOptions: { ssl: { require: true, rejectUnauthorized: false } }, logging: DEBUG }),
+    SUDO: process.env.SUDO || "",
+    LANGUAGE: process.env.LANGUAGE || 'english',
+    DEBUG: DEBUG,
+    ACR_A: "ff489a0160188cf5f0750eaf486eee74",
+    ACR_S: "ytu3AdkCu7fkRVuENhXxs9jsOW4YJtDXimAWMpJp",
+    settingsMenu
+};
